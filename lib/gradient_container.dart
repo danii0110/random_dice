8e1fa27c7e92082f8581ecfcb0fed9534fbe25ac
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-09-14 14:27:01 +0900

    section2-48

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:random_dice/dice_roller.dart';
 
-import 'package:random_dice/styled_text.dart';
-
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  GradientContainer.purple({super.key})
+  const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
-
-  var activeDiceImage =
-      '/Users/dani/Desktop/dani/practice-flutter/random_dice/assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage =
-        '/Users/dani/Desktop/dani/practice-flutter/random_dice/assets/images/dice-4.png';
-  }
 
   @override
   Widget build(context) {
@@ -34,37 +25,11 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(
-                //   top: 20,
-                // ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
 }
-
-
 
 // class GradientContainer extends StatelessWidget {
 //   const GradientContainer({super.key, required this.colors});
